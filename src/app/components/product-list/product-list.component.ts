53b0a79178afa21bedce2fd5dcb72def52198c6f
igor.ptz@hotmail.com <igor.ptz@hotmail.com>
2024-08-06 10:29:17 -0300

    Update

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../common/product';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
+  templateUrl: './product-list-table.component.html',
+  //templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) {}
+  
+  ngOnInit(): void {
+    this.listProducts();
+  }
+
+  listProducts() {
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
 }
