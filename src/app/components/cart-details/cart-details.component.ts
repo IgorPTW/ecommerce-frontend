33ee0cd4410cfeb04c9bfa043f597d49d10f579c
igor.ptz@hotmail.com <igor.ptz@hotmail.com>
2024-09-27 10:07:04 -0300

    Update

---
+++ src/app/components/cart-details/cart-details.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { CartItem } from '../../common/cart-item';
+import { CartService } from '../../services/cart.service';
+
+@Component({
+  selector: 'app-cart-details',
+  templateUrl: './cart-details.component.html',
+  styleUrl: './cart-details.component.css'
+})
+export class CartDetailsComponent implements OnInit {
+
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.listCartDetails();
+  }
+
+  listCartDetails() {
+
+    // Get a handle to the cart items
+    this.cartItems = this.cartService.cartItems;
+
+    // Subscribe to the cart totalPrice
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    )
+
+    // Subscribe to the cart totalQuantity
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    )
+
+    // Compute cart total price and quantity
+    this.cartService.computeCartTotals();
+  }
+}
