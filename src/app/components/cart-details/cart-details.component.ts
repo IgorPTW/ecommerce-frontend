f87df708624fa437904d9a85c56081151f176c79
igor.ptz@hotmail.com <igor.ptz@hotmail.com>
2024-10-01 09:32:32 -0300

    Update

--- src/app/components/cart-details/cart-details.component.ts
+++ src/app/components/cart-details/cart-details.component.ts
@@ -37,4 +37,16 @@ export class CartDetailsComponent implements OnInit {
     // Compute cart total price and quantity
     this.cartService.computeCartTotals();
   }
+
+  incrementQuantity(theCartItem: CartItem) {
+    this.cartService.addToCart(theCartItem);
+  }
+
+  decrementQuantity(theCartItem: CartItem) {
+    this.cartService.decrementQuantity(theCartItem);
+  }
+
+  remove(theCartItem: CartItem) {
+    this.cartService.remove(theCartItem);
+  }
 }
