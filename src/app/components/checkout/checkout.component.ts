ee02a552ef7dfcfd4d1143307cdee5f4e9477946
igor.ptz@hotmail.com <igor.ptz@hotmail.com>
2024-10-02 10:17:18 -0300

    Update

---
+++ src/app/components/checkout/checkout.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrl: './checkout.component.css'
+})
+export class CheckoutComponent implements OnInit {
+
+  checkoutFormGroup: FormGroup = undefined!;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: ['']
+      })
+    });
+  }
+}
